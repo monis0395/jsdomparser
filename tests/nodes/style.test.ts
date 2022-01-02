fd99acf16ce6be2a7be6f97d12f034d7d6bd39f7
monis0395 <monis0395@gmail.com>
2022-01-02 22:48:04 +0530

    updated quotes in style.test.ts

--- tests/nodes/style.test.ts
+++ tests/nodes/style.test.ts
@@ -11,33 +11,33 @@ describe('style', () => {
           // @ts-ignore
           element.style.display,
           'block',
-          "should be able to access value via .display"
+          'should be able to access value via .display'
         );
         assert.equal(
           element.style.getPropertyValue('display'),
           'block',
-          "we can also get value via .getPropertyValue"
+          'we can also get value via .getPropertyValue'
         );
         // @ts-ignore
-        element.style.display = "none"
+        element.style.display = 'none'
         assert.equal(
           // @ts-ignore
           element.style.display,
           'none',
-          "should be able to update value via .display"
+          'should be able to update value via .display'
         );
         element.style.setProperty('display', 'block')
         assert.equal(
           element.style.getPropertyValue('display'),
           'block',
-          "we can also set value via .setProperty"
+          'we can also set value via .setProperty'
         );
         // @ts-ignore
-        element.style.display = "block !important"
+        element.style.display = 'block !important'
         assert.equal(
           element.style.getPropertyValue('display'),
           'block !important',
-          "we can also set value via .setProperty"
+          'we can also set value via .setProperty'
         );
     });
 });
